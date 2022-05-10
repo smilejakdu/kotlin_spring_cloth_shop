7578bf8439e05785c104cdfbf98742c4bc039202
smilejakdu <ash982416@gmail.com>
2022-05-10 12:21:30 +0900

    first settings

---
+++ src/main/java/com/shop/clothshop/ClothshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.clothshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothshopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shop/clothshop/entity/User.kt
@@ -0,0 +1,4 @@
+package com.shop.clothshop.entity
+
+class User(
+)
